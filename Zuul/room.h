6e76841b9adcecb54c0c376eef446275884628ec
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-04-16 09:40:05 -0700

    Create room.h

---
+++ Zuul/room.h
@@ -0,0 +1,27 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+#include <iostream>
+#include <cstring>
+#include <map>
+
+using namespace std;
+
+class Room {
+ public:
+  Room();
+  char* getDescription();
+  int getId();
+  int getItem();
+  map<int, char*>* getExits();
+  void setDescription(char*);
+  void setExits(map<int, char*>);
+  void setId(int);
+  void setItem(int);
+ private:
+  char* description;
+  map<int, char*> exits;
+  int id;
+  int item;
+};
+#endif
