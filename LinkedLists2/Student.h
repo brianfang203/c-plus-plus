95e7c79b366df27ecf1f16981d983b834137250a
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-04-16 10:10:06 -0700

    Create Student.h

---
+++ LinkedLists2/Student.h
@@ -0,0 +1,25 @@
+#ifndef student_h
+#define student_h
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Student {
+ public:
+  Student();
+  ~Student();
+  char* getFirst();
+  char* getLast();
+  int getID();
+  void setID(int);
+  float getGPA();
+  void setGPA(float);
+ private:
+  char first[10];
+  char last[10];
+  int ID;
+  float GPA;
+};
+#endif
