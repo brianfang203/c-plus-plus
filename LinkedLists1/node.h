357cd2f03282d7f747dc871766effca214b157c1
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-04-16 09:50:26 -0700

    Create node.h

---
+++ LinkedLists1/node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include "student.h"
+
+using namespace std;
+
+class Node {
+ public:
+  Node(Student*);
+  ~Node();
+  void setStudent(Student*);
+  void setNext(Node*);
+  Student* getStudent();
+  Node* getNext();
+ private:
+  Student* student;
+  Node* next;
+};
+#endif
