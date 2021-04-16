40010881d2db58ca495d3f9f3a164731839f7287
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-04-16 09:39:42 -0700

    Create room.cpp

---
+++ Zuul/room.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <map>
+#include "room.h"
+
+using namespace std;
+
+Room::Room() {
+
+}
+char* Room::getDescription() {
+  return description;
+}
+
+map<int,char*>* Room::getExits() {
+  return &exits;
+}
+
+int Room::getId() {
+  return id;
+}
+
+int Room::getItem() {
+  return item;
+}
+
+void Room::setDescription(char* newDescription) {
+  description = newDescription;
+}
+
+void Room::setExits(map<int, char*> newExits)  {
+  exits = newExits;
+}
+
+void Room::setId(int newId) {
+  id = newId;
+}
+
+void Room::setItem(int newItem) {
+  item = newItem;
+}
