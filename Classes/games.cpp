6cf3d544a47816841e3ccadbf0f48c21c558b65b
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-04-15 21:32:36 -0700

    Update games.cpp

--- Classes/games.cpp
+++ Classes/games.cpp
@@ -5,9 +5,11 @@
 
 using namespace std;
 
-Game::Game()
-{
-
+Game::Game(char* newTitle, int newYear, char* newPublisher, int newRating){
+     title = newTitle;
+     year = newYear;
+      publisher = newPublisher
+     rating = newRating;
 }
 
 char* Game::getPublisher(){
